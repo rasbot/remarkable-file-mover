1e831354c80e8081c99702e60509f4ab7266fb0e
rasbot <nathan.f.rasmussen@gmail.com>
2024-12-28 14:41:14 -0800

    add output file path function

--- src/crop_resize.py
+++ src/crop_resize.py
@@ -4,6 +4,24 @@ import os
 from PIL import Image
 
 
+def get_processed_output_path(input_path: str, appended_txt: str = "_processed") -> str:
+    """Get the output path of a path string by appending a string to the
+    file name.
+
+    Ex: "my_dir/my_other_dir/my_file.txt" -> "my_dir/my_other_dir/my_file_processed.txt"
+
+    Args:
+        input_path (str): Full path string.
+        appended_txt (str, optional): Text to append to file name.
+            Defaults to "_processed".
+
+    Returns:
+        str: output path string with appended text.
+    """
+    base, ext = os.path.splitext(input_path)
+    return f"{base}{appended_txt}{ext}"
+
+
 def main():
     parser = argparse.ArgumentParser(
         description="Crop and resize an image to specific dimensions."
