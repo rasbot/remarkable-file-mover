6c6fe1dae2d6377fa0da14b4586aa8f5d29c6d0f
Nathan Rasmussen <nathan.f.rasmussen@gmail.com>
2024-12-31 19:01:34 -0800

    add text overlay path, add img dimensions

--- src/constants.py
+++ src/constants.py
@@ -5,5 +5,8 @@ from pathlib import Path
 ROOT_DIR = Path(__file__).resolve().parent.parent
 
 CONFIG_PATH = ROOT_DIR / "config" / "remarkable_config.txt"
+TEXT_OVERLAY_IMAGE_DIR = ROOT_DIR / "text_overlay_images"
 
+TARGET_IMG_DIMS = {"width": 1620, "height": 2160}
+TEXT_OVERLAY_IMG_DIMS = {"width": 810, "height": 720}
 TEXT_IMG_BUFFER = 150
