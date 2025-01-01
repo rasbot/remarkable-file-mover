4561cb0be30dcf4fbdca47d6cf17a9944acd32cf
Nathan Rasmussen <nathan.f.rasmussen@gmail.com>
2025-01-01 10:17:00 -0800

    remove config values

--- src/constants.py
+++ src/constants.py
@@ -8,7 +8,3 @@ CONFIG_DIR = ROOT_DIR / "config"
 CONFIG_PATH = CONFIG_DIR / "remarkable_config.txt"
 IMAGE_CONFIG_PATH = CONFIG_DIR / "image_config.yaml"
 TEXT_OVERLAY_IMAGE_DIR = ROOT_DIR / "text_overlay_images"
-
-TARGET_IMG_DIMS = {"width": 1620, "height": 2160}
-TEXT_OVERLAY_IMG_DIMS = {"width": 810, "height": 720}
-TEXT_IMG_BUFFER = 150
