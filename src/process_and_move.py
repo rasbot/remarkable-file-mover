a493be69d11d60ff554d094e4fa6d6f0889cbccb
Nathan Rasmussen <nathan.f.rasmussen@gmail.com>
2025-01-01 13:45:21 -0800

    update to work with process_image updates, bug fixes

--- src/process_and_move.py
+++ src/process_and_move.py
@@ -3,9 +3,10 @@
 import argparse
 import os
 from pathlib import Path
-from typing import Literal
+from typing import Dict, Literal
 
-from src.constants import CONFIG_PATH
+import src.constants as c
+import src.utils as u
 from src.process_image import get_processed_output_path, process_image, DimensionsDict
 from src.utils import get_config_path, ProtectedFile, ConfigKey
 from src.move_file import move_file
@@ -13,16 +14,20 @@ from src.move_file import move_file
 
 def process_and_move(
     source_path: Path,
-    dimensions_dict: DimensionsDict,
+    img_dims_dict: DimensionsDict,
+    img_config_dict: Dict[str, int],
     position: Literal["center", "left", "right", "top", "bottom"] = "center",
     border: int = None,
+    text_image_path: Path = None,
+    is_inverted: bool = False,
     is_overwritable: bool = False,
 ) -> None:
+    # TODO: update docstring
     """Process an image and move / rename it.
 
     Args:
         source_path (Path): Initial source image path.
-        dimensions_dict (DimensionsDict): Dict of final image dimensions.
+        img_dims_dict (DimensionsDict): Dict of final image dimensions.
         position (Literal["center", "left", "right", "top", "bottom"], optional): Determine
             where to crop the image relative to. Defaults to "center".
         border (int): Border thickness if needed.
@@ -42,13 +47,15 @@ def process_and_move(
 
     process_image(
         image_path=source_path,
-        final_image_dims=dimensions_dict,
+        final_image_dims=img_dims_dict,
+        img_config=img_config_dict,
         crop_position=position,
         border_width=border,
-        save_path=processed_path,
+        text_image_path=text_image_path,
+        is_inverted=is_inverted,
     )
     destination_path = get_config_path(
-        config_path=CONFIG_PATH, config_key=ConfigKey.DESTINATION_DIR
+        config_path=c.CONFIG_PATH, config_key=ConfigKey.SOURCE_PATH
     )
     move_file(
         source_path=processed_path,
@@ -66,12 +73,6 @@ def main():
         "--source", "-s", type=str, help="Path to input image", required=True
     )
     parser.add_argument(
-        "--width", "-w", type=int, default=1620, help="Target width (default: 1620)"
-    )
-    parser.add_argument(
-        "--height", "-ht", type=int, default=2160, help="Target height (default: 2160)"
-    )
-    parser.add_argument(
         "--position",
         "-p",
         choices=["center", "left", "right", "top", "bottom"],
@@ -87,6 +88,20 @@ def main():
         help="Add white border with specified width (default: 30 if flag is used)",
     )
     parser.add_argument(
+        "--textfile",
+        "-t",
+        nargs="?",
+        type=str,
+        const="text_overlay.png",
+        help="File name of text overlay file, if used.",
+    )
+    parser.add_argument(
+        "--invert",
+        "-i",
+        action="store_true",
+        help="Invert the text color (black to white or white to black)",
+    )
+    parser.add_argument(
         "--overwrite",
         "-o",
         action="store_true",
@@ -95,19 +110,28 @@ def main():
 
     args = parser.parse_args()
     input_file_path = args.source
-    width = args.width
-    height = args.height
     position = args.position
     border = args.border
+    text_overlay_filename = args.textfile
+    is_inverted = args.invert
     overwrite = args.overwrite
 
+    text_overlay_filepath = u.get_text_overlay_path(
+        text_overlay_filename=text_overlay_filename
+    )
+    img_config_dict = u.load_config_yaml(c.IMAGE_CONFIG_PATH)
+    width = img_config_dict["target_img_dims"]["width"]
+    height = img_config_dict["target_img_dims"]["height"]
     final_image_dims_dict = DimensionsDict(width=width, height=height)
 
     process_and_move(
         source_path=input_file_path,
-        dimensions_dict=final_image_dims_dict,
+        img_dims_dict=final_image_dims_dict,
+        img_config_dict=img_config_dict,
         position=position,
         border=border,
+        text_image_path=text_overlay_filepath,
+        is_inverted=is_inverted,
         is_overwritable=overwrite,
     )
 
