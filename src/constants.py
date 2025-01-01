dad4d1aa138117b25c28b6ca8d156440e9df4a24
Nathan Rasmussen <nathan.f.rasmussen@gmail.com>
2024-12-31 20:26:01 -0800

    define config paths

--- src/constants.py
+++ src/constants.py
@@ -3,8 +3,10 @@
 from pathlib import Path
 
 ROOT_DIR = Path(__file__).resolve().parent.parent
+CONFIG_DIR = ROOT_DIR / "config"
 
-CONFIG_PATH = ROOT_DIR / "config" / "remarkable_config.txt"
+CONFIG_PATH = CONFIG_DIR / "remarkable_config.txt"
+IMAGE_CONFIG_PATH = CONFIG_DIR / "image_config.yaml"
 TEXT_OVERLAY_IMAGE_DIR = ROOT_DIR / "text_overlay_images"
 
 TARGET_IMG_DIMS = {"width": 1620, "height": 2160}
